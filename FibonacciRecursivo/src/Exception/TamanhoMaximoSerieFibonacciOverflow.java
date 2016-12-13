b357d512a424e8cef37c9f08aeec12e243a607f3
Rodrigo Andrade <rodrigo.andrade@skyneti.com.br>
2016-12-12 22:41:03 -0200

    Alterado para RunTIme

--- FibonacciRecursivo/src/Exception/TamanhoMaximoSerieFibonacciOverflow.java
+++ FibonacciRecursivo/src/Exception/TamanhoMaximoSerieFibonacciOverflow.java
@@ -2,9 +2,6 @@ package Exception;
 
 public class TamanhoMaximoSerieFibonacciOverflow extends RuntimeException {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 4278231382107600307L;
 	
 	public TamanhoMaximoSerieFibonacciOverflow(String message){
